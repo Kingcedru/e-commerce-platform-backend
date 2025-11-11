30c74bda35fb96237b5c40faac4c4a60a18dc2ca
kingcedru <byishimocedrick@gmail.com>
2025-11-11 16:36:46 +0200

    feat: setup models tests

---
+++ tests/setup.ts
@@ -0,0 +1,50 @@
+jest.mock("../src/models/user.model", () => ({
+  User: {
+    findOne: jest.fn(),
+    create: jest.fn(),
+  },
+}));
+
+jest.mock("../src/models/product.model", () => ({
+  Product: {
+    findByPk: jest.fn(),
+    findOne: jest.fn(),
+    findAll: jest.fn(),
+    findAndCountAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    destroy: jest.fn(),
+    decrement: jest.fn(),
+  },
+}));
+
+jest.mock("../src/models/order.model", () => ({
+  Order: {
+    create: jest.fn(),
+    findAll: jest.fn(),
+  },
+}));
+
+jest.mock("../src/models/orderItem.model", () => ({
+  OrderItem: {
+    bulkCreate: jest.fn(),
+  },
+}));
+
+jest.mock("../src/config/database", () => ({
+  default: {
+    transaction: jest.fn(async (callback) => {
+      return callback({
+        commit: jest.fn(),
+        rollback: jest.fn(),
+        LOCK: { UPDATE: "UPDATE" },
+      });
+    }),
+    authenticate: jest.fn(),
+    sync: jest.fn(),
+  },
+}));
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
