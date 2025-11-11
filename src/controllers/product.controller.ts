e084c4b82cd82ff6e64d052b4ac1bd95db3dfb1e
kingcedru <byishimocedrick@gmail.com>
2025-11-11 11:24:48 +0200

    feat: create update product roue

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,9 +1,13 @@
 import { Response, NextFunction } from "express";
 import { Product } from "../models/product.model";
-import { createProductSchema } from "../utils/validation/product.schema";
+import {
+  createProductSchema,
+  updateProductSchema,
+} from "../utils/validation/product.schema";
 import { sendSuccess } from "../utils/response";
 import { BadRequestError } from "../utils/errors/bad-request-error";
 import { AuthenticatedRequest } from "../types/auth";
+import { NotFoundError } from "@/utils/errors/not-found-error";
 
 export const createProduct = async (
   req: AuthenticatedRequest,
@@ -24,6 +28,36 @@ export const createProduct = async (
     });
 
     sendSuccess(res, 201, "Product created successfully.", newProduct);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateProduct = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { error, value } = updateProductSchema.validate(req.body);
+  if (error) {
+    return next(new BadRequestError(error.details[0].message));
+  }
+
+  const productId = req.params.id;
+
+  try {
+    const product = await Product.findByPk(productId);
+
+    if (!product) {
+      return next(new NotFoundError(`Product with ID ${productId} not found.`));
+    }
+
+    const [, [updatedProduct]] = await Product.update(value, {
+      where: { id: productId },
+      returning: true,
+    });
+
+    sendSuccess(res, 200, "Product updated successfully.", updatedProduct);
   } catch (err) {
     next(err);
   }
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createProduct } from "../controllers/product.controller";
+import {
+  createProduct,
+  updateProduct,
+} from "../controllers/product.controller";
 import { authenticate } from "../middleware/auth.middleware";
 import { isAdmin } from "../middleware/role.middleware";
 
 const router = Router();
 
 router.post("/", authenticate, isAdmin, createProduct);
+router.put("/:id", authenticate, isAdmin, updateProduct);
 
 export default router;
